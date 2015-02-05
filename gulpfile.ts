eb3cf8d1d5281fdf68fff88142e640bb153af2c6
Chitoku <info@chitoku.jp.pn>
2015-02-05 14:54:15 +0900

    Modify: Support error notifiers for TypeScript and SCSS

--- gulpfile.ts
+++ gulpfile.ts
@@ -7,6 +7,15 @@ import browserSync = require("browser-sync");
 var $ = require("gulp-load-plugins")();
 var runSequence = require("run-sequence");
 
+class ErrorNotifier {
+    public static getErrorListener(title: string): (error: Error) => void {
+        return $.notify.onError((error: Error) => ({
+            title: title,
+            message: error.message.replace(/\u001b\[\d+m/g, "")
+        }));
+    }
+}
+
 class Tasks {
     [id: string]: gulp.ITaskCallback;
 
@@ -28,6 +37,7 @@ class Tasks {
                        target: "ES5",
                        sortOutput: true
                    })).js
+                   .on("error", ErrorNotifier.getErrorListener("TypeScript Compilation Error"))
                    .pipe($.sourcemaps.write("."))
                    .pipe(gulp.dest("./dev/js/"))
                    .pipe($.size());
@@ -56,9 +66,9 @@ class Tasks {
     private styles(): NodeJS.ReadWriteStream {
         return gulp.src(["./src/scss/style.scss", "./lib/css/*.css", "!./lib/css/*.min.css"])
                    .pipe($.sass({
-                       precision: 10,
-                       onError: console.error.bind(console, "Sass error:")
+                       precision: 10
                    }))
+                   .on("error", ErrorNotifier.getErrorListener("SCSS Compilation Error"))
                    .pipe(gulp.dest("dev/css"))
                    .pipe($.size({ title: "styles" }));
     }
