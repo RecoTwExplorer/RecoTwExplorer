6aedfafd3776190588d9cdc6f2b252ceb4c9fcfc
hina <hina@hinaloe.net>
2015-02-05 15:49:41 +0900

    Bump to ver 2.2.1

--- src/ts/recotw.ts
+++ src/ts/recotw.ts
@@ -23,7 +23,7 @@
 
 module RecoTwExplorer {
     var APP_NAME = "RecoTw Explorer";
-    var APP_VERSION = "2.20";
+    var APP_VERSION = "2.21";
 
     /*
      * Bootstrap jQuery Shake Effect snippet - pAMmDzOfnL
