93ae3cb7e31cad4e5e97f69f5e8452920aa1b090
Chitoku <info@chitoku.jp.pn>
2015-02-17 22:13:54 +0900

    Bump to ver 2.33

--- src/ts/recotw.ts
+++ src/ts/recotw.ts
@@ -23,7 +23,7 @@
 
 module RecoTwExplorer {
     var APP_NAME = "RecoTw Explorer";
-    var APP_VERSION = "2.32";
+    var APP_VERSION = "2.33";
 
     /*
      * Bootstrap jQuery Shake Effect snippet - pAMmDzOfnL
