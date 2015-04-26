e1bcc99b98c354687ef29adb5311e7032522665e
hina <hina@hinaloe.net>
2015-04-27 00:05:37 +0900

    Corresponds to the new design (# 25), in the process

--- src/ts/recotw.ts
+++ src/ts/recotw.ts
@@ -798,7 +798,8 @@ module RecoTwExplorer {
             if (!element) {
                 View.showStatusLoadFailedMessage(widgetID, entry);
             } else {
-                $(element).contents().find(".standalone-tweet, .multi-photo .crop-media-box").css({ borderRadius: 0 });
+                $(element).contents().find(".EmbeddedTweet, .mediacard-borderoverlay, .ImageGrid-image").css({ borderRadius: 0 });
+                $(element).contents().find(".Tweet-followButton").hide();
             }
         }
 
