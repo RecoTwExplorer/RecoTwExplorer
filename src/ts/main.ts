01b68f27076eef5ccec6bd441a34a01823b08916
Chitoku <odango@chitoku.jp>
2015-06-11 04:24:48 +0900

    Close #16: Check if registered and show the warning message

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -88,13 +88,16 @@ module RecoTwExplorer {
      * The resources for UI.
      */
     class Resources {
-        public static INCORRECT_REGEX = "指定された正規表現は正しくありません。";
-        public static INCORRECT_URL_OR_ID = "指定された URL または ID は正しくありません。";
-        public static BADGE_NOT_SUPPORTED = "通知バッジがサポートされていません。";
-        public static FAILED_TO_GENERATE_STATUS_URL = "ツイートの URL を生成できません。";
-        public static FAILED_TO_GENERATE_USER_URL = "ユーザーへの URL を生成できません。";
-        public static FAILED_TO_GENERATE_PROFILE_IMAGE_URL = "プロフィール画像の URL を生成できません。";
-        public static FAILED_TO_LOAD_EMBEDDED_TWEET = "Twitter 埋め込みツイートの読み込みに失敗しました。";
+        public static INCORRECT_REGEX = "指定された正規表現は正しくありません";
+        public static INCORRECT_URL_OR_ID = "指定された URL または ID は正しくありません";
+        public static ALREADY_REGISTERED = "すでに登録されているツイートです";
+        public static REGISTRATION_AVAILABLE = "このツイートは登録可能です";
+        public static REGISTRATION_DEFAULT = "ツイートの ID または URL を指定します";
+        public static BADGE_NOT_SUPPORTED = "通知バッジがサポートされていません";
+        public static FAILED_TO_GENERATE_STATUS_URL = "ツイートの URL を生成できません";
+        public static FAILED_TO_GENERATE_USER_URL = "ユーザーへの URL を生成できません";
+        public static FAILED_TO_GENERATE_PROFILE_IMAGE_URL = "プロフィール画像の URL を生成できません";
+        public static FAILED_TO_LOAD_EMBEDDED_TWEET = "Twitter 埋め込みツイートの読み込みに失敗しました";
         public static REGISTER_NEW_TWEET = "ツイートの ID または URL:";
         public static REGISTRATION_FAILED_HTML = "<strong>登録失敗</strong><br>{0}";
         public static SEARCH_HELP_HTML = "<dl><dt>ツイート検索</dt><dd><code>/</code> と <code>/</code> で囲むと正規表現検索</dd><dt>ユーザー名検索</dt><dd><code>from:</code> でユーザーを検索<br>カンマ区切りで複数入力</dd><dt>ID 検索</dt><dd><code>id:</code> で ID 検索</dd></dl>";
@@ -116,12 +119,12 @@ module RecoTwExplorer {
             [code: string]: string;
             UNKNOWN_ERROR: string;
         } = {
-            "400": "パラメーターが正しくありません。",
-            "403": "ツイートの取得に失敗しました。",
-            "404": "指定されたツイートは存在しません。",
-            "500": "すでに登録されているツイートです。",
-            "503": "サーバーで問題が発生しています。",
-            UNKNOWN_ERROR: "原因不明のエラーです。"
+            "400": "パラメーターが正しくありません",
+            "403": "ツイートの取得に失敗しました",
+            "404": "指定されたツイートは存在しません",
+            "500": Resources.ALREADY_REGISTERED,
+            "503": "サーバーで問題が発生しています",
+            UNKNOWN_ERROR: "原因不明のエラーです"
         };
     }
 
@@ -1250,9 +1253,7 @@ module RecoTwExplorer {
             $("#new-record-modal").on("hidden.bs.modal", () => {
                 $("#new-record-form .modal-body").empty().html(Resources.URL_INPUT_AREA);
             });
-            $("#new-record-modal").on("keyup change input", ".url-box", () => {
-                Controller.onNewRecordFormTextBoxValueChanged();
-            });
+            $("#new-record-modal").on("keyup change input", ".url-box", Controller.onNewRecordFormTextBoxValueChanged);
             $("#statistics-filter-textbox").on("keyup change input", ($event: JQueryEventObject) => {
                 Tab.statistics.applyChartFilter((<HTMLInputElement>$event.target).value);
             });
@@ -1356,34 +1357,37 @@ module RecoTwExplorer {
 
         public static onChartSliceClick(slice: any): void {
             var target = <string>slice.targetID;
-            if (!target.contains("#")) {
+            var index = target.indexOf("#");
+            if (index < 0) {
                 return;
             }
-            var id = +target.substring(target.indexOf("#") + 1);
+            var id = +target.substring(index + 1);
             if (id < 0) {
                 return;
             }
             Tab.statistics.applySearchFilter(Model.statistics.users[id].target_sn);
         }
 
-        private static onNewRecordFormTextBoxValueChanged(): void {
-            var $elements = $(".url-input-area");
-            var inputs: string[] = $(".url-box").map((index: number, element: HTMLInputElement) => element.value).get();
-
-            $elements.each(function (index) {
-                $(this).removeClass("has-success has-error");
-                try {
-                    if (Model.createIDfromURL(inputs[index]) !== null) {
-                        $(this).addClass("has-success");
-                    }
-                } catch (e) {
-                    $(this).addClass("has-error");
+        private static onNewRecordFormTextBoxValueChanged($event: JQueryEventObject): void {
+            var $this = $(this).parents(".url-input-area").removeClass("has-success has-warning has-error");
+            var elm = <HTMLInputElement>$event.target;
+            try {
+                var id = Model.createIDfromURL(elm.value);
+                if (Model.entries.enumerable.any(x => x.tweet_id === id)) {
+                    $this.addClass("has-warning").find(".help-block").text(Resources.ALREADY_REGISTERED);
+                } else if (id !== null) {
+                    $this.addClass("has-success").find(".help-block").text(Resources.REGISTRATION_AVAILABLE);
+                } else {
+                    $this.find(".help-block").text(Resources.REGISTRATION_DEFAULT);
                 }
-            });
+            } catch (e) {
+                $this.addClass("has-error").find(".help-block").text(Resources.INCORRECT_URL_OR_ID);
+            }
 
             /**
              * [FUTURE] The following code is to increase text boxes for multiple entries registration.
              */
+            // var inputs: string[] = $(".url-box").map((index: number, element: HTMLInputElement) => element.value).get();
             // if (inputs[inputs.length - 1] !== "") {
             //     $("#new-record-form .modal-body").append(Resources.URL_INPUT_AREA);
             // } else if (inputs.length >= 2 && inputs[inputs.length - 2] === "") {
