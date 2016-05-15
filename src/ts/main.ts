970425d653b84f24a130a6cbad7d3fc18d94e252
Chitoku <odango@chitoku.jp>
2016-05-16 02:47:08 +0900

    Fix to use ||

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -957,7 +957,7 @@ module RecoTwExplorer {
                 if (!widget) {
                     this.showStatusLoadFailedMessage(entry, $element);
                 } else {
-                    const $contents = $(widget.shadowRoot ? widget.shadowRoot : widget).css("height", "auto").contents();
+                    const $contents = $(widget.shadowRoot || widget).css("height", "auto").contents();
                     $contents.find(".Tweet-brand .u-hiddenInNarrowEnv").hide();
                     $contents.find(".Tweet-brand .u-hiddenInWideEnv").css("display", "inline-block");
                     $contents.find(".Tweet-author").css("max-width", "none");
